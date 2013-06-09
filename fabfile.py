f62fdac927fa6dc5f40035f7e5051e8d1d4262f8
Maslino <liuxionghust@gmail.com>
2013-06-09 11:46:52 +0800

    improve upgrade task

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,9 @@ fabfile for Fabric
 """
 import os
 from fabric.api import *
-
+from fabric.contrib.files import exists
 
 env.roledefs = {
-    "local": (
-	'localhost',
-    ),
     "namenode": (
         "hadoop@nn",
     ),
@@ -24,23 +21,16 @@ env.roledefs = {
     )
 }
 
-
-@roles("local")
 def local_uname():
     local("uname -a")
 
-
 @roles('namenode')
 def nn_uname():
-    env.key_filename = "/home/liuxiong/.ssh/id_rsa.pub"
     run("uname -a")
-
 
 @roles("datanode")
 def dn_uname():
-    env.key_filename = "/home/hadoop/.ssh/id_rsa.pub"
     run("uname -a")
-
 
 @roles("datanode")
 def exec_cmd(command_string):
@@ -49,43 +39,64 @@ def exec_cmd(command_string):
 ###################################################################
 
 def install_jdk7():
+    """
+    在所有节点上安装jdk7
+    """
     # assume jdk7 tarball is under directory ~/download
-    jdk7_tarball = "/home/hadoop/download/jdk-7u21-linux-x64.tar.gz"
+    download_dir = "/home/hadoop/download"
+    if not exists(download_dir):
+        run("mkdir %s"%download_dir)
+
+    jdk7_tarball = os.path.join(download_dir, "jdk-7u21-linux-x64.tar.gz")
+    if not exists(jdk7_tarball):
+        raise Exception("jdk7 tarball not found in directory: %s"%download_dir)
+
     target_dir = "/usr/java"
     with cd(target_dir):
-        sudo("tar zxf %s"%jdk7_tarball)
+        sudo("tar -zxf %s"%jdk7_tarball)
         for link in ["jdk", "latest"]:
             sudo("rm %s"%link)
-            sudo("ln -s %s %s"%(target_dir + "/jdk1.7.0_21", link))
+            sudo("ln -s %s %s"%(os.path.join(target_dir, "jdk1.7.0_21"), link))
 
 ###################################################################
 
 def stop_hbase():
-    stop_script = "/home/hadoop/hbase-single/bin/start-hbase.sh"
+    """
+    在所有datanode节点上停止hbase
+    """
+    stop_script = "/home/hadoop/hbase-single/bin/stop-hbase.sh"
     run("%s"%stop_script)
     
-    
 def check_hbase():
+    """
+    在所有datanode节点上检查hbase是否已被停止
+    """
     output = run("jps")
     for daemon in ["hmaster", "hregionserver", "hquorumpeer"]:
         assert daemon not in output.lower()
 
-
 def stop_namenode():
-    namenode_service = ""
-    jobtracker_service = ""
+    """
+    停止namenode节点上的namenode和jobtracker服务
+    """
+    namenode_service = "hadoop-0.20-namenode"
+    jobtracker_service = "hadoop-0.20-jobtracker"
     sudo("service %s stop"%namenode_service)
     sudo("service %s stop"%jobtracker_service)
 
-
 def stop_datanode():
-    datanode_service = ""
-    tasktracker_service = ""
+    """
+    停止datanode节点上的datanode和tasktracker服务
+    """
+    datanode_service = "hadoop-0.20-datanode"
+    tasktracker_service = "hadoop-0.20-tasktracker"
     sudo("service %s stop"%datanode_service)
     sudo("service %s stop"%tasktracker_service)
 
-
 def check_hadoop():
+    """
+    检查hdfs和mapreduce服务是否停止
+    """
     output = run("jps")
     for daemon in ["datanode", "tasktracker", "jobtracker", "namenode"]:
         assert daemon not in output.lower()
@@ -93,67 +104,110 @@ def check_hadoop():
 #################################################################
 
 def backup_hdfs_metadata():
+    """
+    在namenode节点上备份hdfs的元数据
+    """
     metadata_dir = "/data/hadoop/cache/hadoop/dfs/name"
+    assert exists(metadata_dir)
     backup_dir = "/home/hadoop/backup"
-    assert "lock" not in local("ls %s"%metadata_dir).lower()
-    local("mkdir %s"%backup_dir)
-    local("tar cvf %s %s"%(backup_dir + "/namenode_backup_metadata.tar", metadata_dir))
+    if not exists(backup_dir):
+        run("mkdir %s"%backup_dir)
+
+    assert "lock" not in run("ls %s"%metadata_dir).lower()
+
+    run("tar -cvf %s %s"%(
+        os.path.join(backup_dir, "namenode_backup_metadata.tar"), metadata_dir))
     
-        
 def backup_hadoop_conf():
+    """
+    备份所有节点上的配置文件
+    """
     orig_conf_dir = "/etc/hadoop/conf"
     backup_conf_dir = "/etc/hadoop/conf.cdh4"
-    sudo("cp -r %s %s"%(orig_conf_dir, backup_dir_conf))
+    run("cp -r %s %s"%(orig_conf_dir, backup_conf_dir))
     sudo("update-alternatives --install %s hadoop-conf %s 50"%(orig_conf_dir, backup_conf_dir))
-    output = run("alternatives --display hadoop-conf")
-    assert "something is right"
 
+    output = run("alternatives --display hadoop-conf")
+    lines = output.split("\n")
+    assert backup_conf_dir in lines[1]
 
 ################################################################
 
 def uninstall_cdh3():
-    sudo("yum remove hadoop-0.20 bigtop-utils")
+    """
+    在所有节点上卸载cdh3
+    """
+    sudo("yum remove hadoop-0.20")
+    sudo("yum remove bigtop-utils")
     sudo("yum remove cloudera-cdh3")
 
+    output = run("rpm -qa | grep -i hadoop")
+    if output:
+        assert "hadoop" not in output.lower()
+
 def install_cdh4():
-    cdh_rpm_url = "http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm"
+    """
+    在所有节点上安装cdh4
+    """
+    cdh4_rpm_url = "http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm"
     download_dir = "/home/hadoop/download"
-    run("mkdir %s"%download_dir)
+    if not exists(download_dir):
+        run("mkdir %s"%download_dir)
+
     with cd(download_dir):
-        run("wget %s"%cdh_rmp_url)
+        run("wget %s"%cdh4_rpm_url)
         sudo("yum --nogpgcheck localinstall cloudera-cdh-4-0.x86_64.rpm")
-        sudo("rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera")
-        sudo("yum install hadoop-0.20-mapreduce-jobtracker")
-        sudo("yum install hadoop-hdfs-namenode")
-        sudo("yum install hadoop-0.20-mapreduce-tasktracker")
-        sudo("yum install hadoop-hdfs-datanode")
+
+    sudo("rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera")
+    sudo("yum install hadoop-0.20-mapreduce-jobtracker")
+    sudo("yum install hadoop-hdfs-namenode")
+    sudo("yum install hadoop-0.20-mapreduce-tasktracker")
+    sudo("yum install hadoop-hdfs-datanode")
 
 #################################################################
 
-def copy_log4j_properties():
-    run("cp /etc/hadoop/conf.empty/log4j.properties\
-        /etc/hadoop/conf.cdh4/log4j.properties")
+def replace_log4j_properties():
+    """
+    在所有节点上，用cdh4的log4j配置文件覆盖老版本的log4j配置文件
+    """
+    run("cp /etc/hadoop/conf.dist/log4j.properties /etc/hadoop/conf.cdh4/log4j.properties")
+
+def recover_hadoop_conf():
+    """
+    在所有节点上，使用原先备份的配置文件
+    """
+    #todo
+    pass
 
 #################################################################
         
 def upgrade_hdfs():
+    """
+    升级hdfs， 在namenode节点上执行
+    等待升级完成
+    """
     sudo("service hadoop-hdfs-namenode upgrade")
 
 def start_datanode():
+    """
+    启动所有datanode节点
+    等待namenode退出安全模式
+    """
     sudo("service hadoop-hdfs-datanode start")
 
 def start_tasktracker():
+    """
+    在每个datanode上启动TaskTracker
+    """
     sudo("service hadoop-0.20-mapreduce-tasktracker start")
     assert "tasktracker" in run("jps").lower()
     
 def start_jobtracker():
+    """
+    在namenode节点上启动JobTracker
+    """
     sudo("service hadoop-0.20-mapreduce-jobtracker start")
     assert "jobtracker" in run("jps").lower()
-
-##################################################################
-
-def recover_hadoop_conf():
-    pass
 
 #################################################################
 
@@ -161,5 +215,4 @@ def restart_cluster():
     pass
 
 def finalize():
-    sudo("hdfs dfsadmin -finalizeUpgrade")
-
+    run("hdfs dfsadmin -finalizeUpgrade")
