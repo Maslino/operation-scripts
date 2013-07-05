b064cca0f7b3bd33cd46931422a7654ebd0175dc
Maslino <liuxionghust@gmail.com>
2013-07-05 15:43:20 +0800

    new clean log script

---
+++ config/roles.py
@@ -0,0 +1,42 @@
+# coding=utf8
+
+"""
+define some roles for Fabric
+"""
+from fabric.api import env
+
+
+PRODUCTION_NAMENODE = "pro-nn"
+PRODUCTION_DATANODE = "pro-dn"
+PRODUCTION_DATANODE_NEW = "pro-dn-new"
+
+DEVELOPMENT_NAMENODE = "dev-nn"
+DEVELOPMENT_DATANODE = "dev-dn"
+
+
+env.roledefs = {
+    PRODUCTION_NAMENODE: (
+        "hadoop@pro-nn",
+    ),
+    PRODUCTION_DATANODE: (
+        "hadoop@192.168.1.147",
+        "hadoop@192.168.1.148",
+        "hadoop@192.168.1.150",
+        "hadoop@192.168.1.151",
+        "hadoop@192.168.1.152",
+        "hadoop@192.168.1.154",
+    ),
+    PRODUCTION_DATANODE_NEW: (
+        "hadoop@192.168.1.23",
+        "hadoop@192.168.1.24",
+        "hadoop@192.168.1.29",
+        "hadoop@192.168.1.31",
+    ),
+    DEVELOPMENT_NAMENODE: (
+        "hadoop@10.1.16.221",
+    ),
+    DEVELOPMENT_DATANODE: (
+        "hadoop@10.1.16.221",
+        "hadoop@10.1.16.222",
+    ),
+}
---
+++ log/clean_log.py
@@ -0,0 +1,96 @@
+# coding=utf8
+
+"""
+clean log for HBase and Hadoop
+"""
+import os
+import datetime
+from fabric.api import sudo, local, run, execute
+from fabric.contrib.files import exists
+from config.roles import *
+
+
+HDFS_LOG_DIR = "/var/log/hadoop-hdfs"
+MAPRED_LOG_DIR = "/var/log/hadoop-0.20-mapreduce"
+MAPRED_HISTORY_DIR = os.path.join(MAPRED_LOG_DIR, "history")
+HBASE_LOG_DIR = "/home/hadoop/hbase-single/logs"
+
+
+def __arg2bool(arg):
+    if isinstance(arg, bool):
+        return arg
+    if arg.lower() == "false":
+        return False
+    if arg.lower() == "true":
+        return True
+
+    raise Exception("Invalid arg: %s" % arg)
+
+
+def __clean_hdfs_log(remote, delete=False):
+    remote = __arg2bool(remote)
+    delete = __arg2bool(delete)
+    cmd = "find %s -type f -mtime 7 -name 'hadoop-hdfs-*-ELEX-LA-*.log.*'" % (HDFS_LOG_DIR, )
+    if delete:
+        cmd = "%s -delete" % cmd
+
+    if remote:
+        if exists(HDFS_LOG_DIR):
+            sudo(cmd)
+    else:
+        if os.path.exists(HDFS_LOG_DIR):
+            local("sudo %s" % cmd)
+
+
+def __clean_mapred_log(remote, delete=False):
+    remote = __arg2bool(remote)
+    delete = __arg2bool(delete)
+    cmd = "find %s -type f -mtime 7 -name 'hadoop-hadoop-*-ELEX-LA-*.log.*'" % (MAPRED_LOG_DIR, )
+    if delete:
+        cmd = "%s -delete" % cmd
+
+    if remote:
+        if exists(MAPRED_LOG_DIR):
+            sudo(cmd)
+    else:
+        if os.path.exists(MAPRED_LOG_DIR):
+            local("sudo %s" % cmd)
+
+
+def __clean_hbase_log(delete=False):
+    delete = __arg2bool(delete)
+    cmd = "find %s -type f -mtime 7 -name 'hbase-hadoop-*-ELEX-LA-*.log.*'" % (HBASE_LOG_DIR, )
+    if delete:
+        cmd = "%s -delete" % cmd
+
+    if exists(HBASE_LOG_DIR):
+        run(cmd)
+
+
+def __clean_mapred_history(delete=False):
+    delete = __arg2bool(delete)
+    thirty_days_ago = datetime.datetime.today() - datetime.timedelta(days=30)
+
+    # 删除30天前的的历史记录
+    target_dir = os.path.join(MAPRED_HISTORY_DIR, "done/ELEX-LA-WEB1_1372484246521_")
+    last_month_dir = os.path.join(target_dir, "%d" % thirty_days_ago.year, "%02d" % (thirty_days_ago.month - 1, ))
+
+    for day in range(1, thirty_days_ago.day):
+        day_dir = os.path.join(last_month_dir, "%02d" % day)
+        if not os.path.exists(day_dir):
+            print "%s not exists." % day_dir
+            continue
+
+        if delete:
+            local("sudo rm -rf %s" % day_dir)
+        else:
+            local("sudo ls -lR %s" % day_dir)
+
+
+def do_clean(delete=False):
+    execute(__clean_hbase_log, delete, role=PRODUCTION_DATANODE)
+    execute(__clean_hdfs_log, False, delete)
+    execute(__clean_hdfs_log, True, delete, roles=(PRODUCTION_DATANODE, PRODUCTION_DATANODE_NEW))
+    execute(__clean_mapred_log, False, delete)
+    execute(__clean_mapred_log, True, delete, roles=(PRODUCTION_DATANODE, PRODUCTION_DATANODE_NEW))
+    execute(__clean_mapred_history, delete)
