991a9a88e6f6dc1e183476a96640df743fb35690
Maslino <liuxionghust@gmail.com>
2013-07-09 14:52:42 +0800

    add new datanode

--- config/roles.py
+++ config/roles.py
@@ -19,17 +19,20 @@ env.roledefs = {
         "hadoop@pro-nn",
     ),
     PRODUCTION_DATANODE: (
+        "hadoop@192.168.1.146"
         "hadoop@192.168.1.147",
         "hadoop@192.168.1.148",
         "hadoop@192.168.1.150",
         "hadoop@192.168.1.151",
         "hadoop@192.168.1.152",
+        "hadoop@192.168.1.153"
         "hadoop@192.168.1.154",
     ),
     PRODUCTION_DATANODE_NEW: (
         "hadoop@192.168.1.23",
         "hadoop@192.168.1.24",
         "hadoop@192.168.1.29",
+        "hadoop@192.168.1.30"
         "hadoop@192.168.1.31",
     ),
     DEVELOPMENT_NAMENODE: (
