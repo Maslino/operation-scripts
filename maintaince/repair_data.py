812faa8972ccfe6b0d79ed3afc25e7311ad1e184
Maslino <liuxionghust@gmail.com>
2013-11-07 17:51:17 +0800

    wait repair process to complete

--- maintaince/repair_data.py
+++ maintaince/repair_data.py
@@ -5,8 +5,12 @@
 """
 
 import os
+import sys
+import time
+import commands
 from datetime import datetime
 from contextlib import closing
+
 
 CONFIG_PROPERTIES = "config.properties"
 SEND_LOG_PROGRESS = "sendlog.process"
@@ -22,6 +26,8 @@ CONFIG_DAY = "day"
 
 DATETIME_FORMAT_ONE = "%Y%m%d%H%M%S"                # 20131031180000
 DATETIME_FORMAT_TWO = "%a %b %d %H:%M:%S %Z %Y"     # Wed Oct 30 11:41:40 CST 2013
+
+SLEEP_INTERVAL_SECONDS = 30
 
 
 def find_start_pos(start_date_string):
@@ -73,11 +79,57 @@ def update_event_repair_config(start_date_string):
         progress_file.flush()
 
 
+def check_if_complete(stop_date_string):
+    day, stop_progress = find_stop_pos(stop_date_string)
+
+    with closing(open(os.path.join(EVENT_REPAIR_CONFIG_DIR, SEND_LOG_PROGRESS), "r")) as progress_file:
+        lines = progress_file.readlines()
+        for line in reversed(lines):
+            line = line.strip()
+            items = line.split("\t")
+            if len(items) != 3:
+                continue
+            if items[0] == day and long(items[1]) >= long(stop_progress):
+                return True
+            else:
+                return False
+
+
+def kill_repair_process():
+    cmd = """ps -ef | grep 'com.xingcloud.server.DataLoaderFlush16RepairWather' | grep -v grep | awk '{print $2}'"""
+    status, pid = commands.getstatusoutput(cmd)
+    if status != 0:
+        print "command execute error: ", cmd
+        return
+
+    print pid
+    assert pid and len(pid.strip().split("\n")) == 1
+
+    kill_cmd = "kill " + pid
+    status, _ = commands.getstatusoutput(kill_cmd)
+    if status != 0:
+        print "command execute error: ", kill_cmd
+
+
 if __name__ == "__main__":
-    day, start_progress = find_start_pos("20131107060000")
-    day, stop_progress = find_stop_pos("20131107123000")
+    if len(sys.argv) != 3:
+        print "Usage: \n\t", sys.argv[0], "start_date", "stop_date"
+        exit(-1)
 
-    print "start: ", day, start_progress
-    print "stop: ", day, stop_progress
+    start_date_string = sys.argv[1]
+    stop_date_string = sys.argv[2]
+    start_day, start_progress = find_start_pos(start_date_string)
+    stop_day, stop_progress = find_stop_pos(stop_date_string)
 
-    update_event_repair_config("20131107060000")
+    print "start: ", start_day, start_progress
+    print "stop: ", stop_day, stop_progress
+
+    update_event_repair_config(start_date_string)
+
+    time.sleep(SLEEP_INTERVAL_SECONDS)
+
+    while not check_if_complete(stop_date_string):
+        print "waiting..."
+        time.sleep(SLEEP_INTERVAL_SECONDS)
+
+    kill_repair_process()
